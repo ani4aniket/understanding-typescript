e7f9bf5d2b260a37b44501af942c9b8d69c8d1a7
Aniket Kumar <aniketkumar049@gmail.com>
2020-02-28 00:07:52 +0530

    unknown and never added

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log('Result' + num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-printResult(add(1, 2));
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-var combineValues;
-combineValues = add;
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+var result = generateError('An error occured!', 500);
+console.log("ger", result);
